8f19e9835947cd446da9b5a42b2e0abb9144fb56
giacomoverdura <bs.jamesgulay@gmail.com>
2026-05-01 14:17:30 +0800

    create sections files

--- saas-landing-page/src/App.tsx
+++ saas-landing-page/src/App.tsx
@@ -1,9 +1,28 @@
 import './App.css'
+import Navbar from './components/sections/Navbar';
+import Hero from './components/sections/Hero';
+import Trusted from './components/sections/Trusted';
+import Features from './components/sections/Features';
+import HowItWorks from './components/sections/HowItWorks';
+import Pricing from './components/sections/Pricing';
+import Testimonials from './components/sections/Testimonials';
+import FAQ from './components/sections/FAQ';
+import CTA from './components/sections/CTA';
+import Footer from './components/sections/Footer';
 
 function App() {
   return (
     <>
-    
+      <Navbar />
+      <Hero />
+      <Trusted />
+      <Features />
+      <HowItWorks />
+      <Pricing />
+      <Testimonials />
+      <FAQ />
+      <CTA />
+      <Footer />
     </>
   )
 }
